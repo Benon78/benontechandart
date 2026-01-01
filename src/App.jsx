1cfcfa560efdef87a9727809af47f499a55f596a
Benon78 <wilbenjamin7@gmail.com>
2026-01-01 23:46:50 +0300

    fix: add the error boundary element

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,35 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
+import ErrorElement from "./pages/ErrorElement";
 
 import './App.css'
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Index />} errorElement={<ErrorElement />} />
+      <Route path="/auth" element={<Auth />} errorElement={<ErrorElement />} />
+      <Route path="/admin" element={<Admin />} errorElement={<ErrorElement />} />
+      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL */}
+      <Route path="*" element={<NotFound />} />
+    </>
+  )
+);
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/admin" element={<Admin />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </TooltipProvider>
   </QueryClientProvider>
 );
